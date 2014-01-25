0dd8d0cd904034be2c9d569723c0af8d6bb67c4d
alex649 <alexalex649@gmail.com>
2014-01-25 18:43:05 -0500

    Third Commit

--- controller/sell.php
+++ controller/sell.php
@@ -1,27 +1,29 @@
 <?php
 /*********************
- * buy.php
+ * sell.php
  *
  * CSCI S-75
  * Project 1
  * Chris Gerber
  *
- * Buy shares controller
+ * Sell shares controller
  *********************/
 
 require_once('../model/model.php');
 require_once('../includes/helper.php');
 
+$symbol;
+
 if (isset($_SESSION['userid']))
 {
     $userid = (int)$_SESSION['userid'];
-    $symbol = $_POST['symbol'];
-    // $name = $_POST['name'];
-    $last_trade = $_POST['last_trade'];
-    $shares = $_POST['shares'];
+
+    foreach($_POST as $key=>$value) {
+        $symbol = $key;
+    }
 
     // buy shares
-    buy_shares($userid, $symbol, $last_trade, $shares, &$error);
+    sell_shares($userid, $symbol, &$error);
 
     // get the list of holdings for user
     $holdings = get_user_shares($userid);
--- view/portfolio.php
+++ view/portfolio.php
@@ -4,17 +4,19 @@ render('header', array('title' => 'Portfolio'));
 ?>
 
 <h1>Portfolio</h1>
+<form action=sell method=POST>
 <table>
     <tr>
         <th>Symbol</th>
         <th>Shares</th>
     </tr>
 <?php
+
 foreach ($holdings as $holding)
 {
     print "<tr>";
     print "<td>" . htmlspecialchars($holding["symbol"]) . "</td>";
-    print "<td>" . htmlspecialchars($holding["shares"]) . "</td>";
+    print "<td name=" . $holding["symbol"] . ">" . htmlspecialchars($holding["shares"]) . "</td>";
     print "<td>" . "<input type=submit name=" . $holding['symbol'] . " ";
     print "value='Sell'>";
     print "</td>";
@@ -22,6 +24,7 @@ foreach ($holdings as $holding)
 }
 ?>
 </table>
+</form>
 
 <?php
 render('footer');
